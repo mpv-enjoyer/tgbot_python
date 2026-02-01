ac0a7026bc1a676058bdcc5e60d145788440cbb5
Alex <Disblocker.just@gmail.com>
2026-02-01 19:50:36 +0300

    notif left

--- birthday.py
+++ birthday.py
@@ -5,60 +5,89 @@ import datetime
 
 TABLE_CALLERS = "callers"
 TABLE_BIRTHDAYS = "birthdays"
-BIRTHDAYS_DB_FILE_PATH = "birthdays.db"
 LIMIT_BIRTHDAYS_PER_CALLER = 50
-DEBUG_CALLER = "123456789"
 DATE_FORMAT = "%d.%m.%y"
-TIME_FORMAT = "%H"
 SQL_DATE_FORMAT = "%Y-%m-%d"
+BIRTHDAYS_DB_FILE_PATH = "birthdays.db"
 
-# Returns list(caller_id, birthday, comment)
-def get_birthdays(cur: sql.Cursor, caller_id: int):
-    res = cur.execute(f"SELECT * FROM {TABLE_BIRTHDAYS} WHERE caller_id=?", (caller_id,)).fetchall()
-    res2 = cur.execute(f"SELECT update_on FROM {TABLE_CALLERS} WHERE caller_id=?", (caller_id,)).fetchone()
+cur: sql.Cursor
+con: sql.Connection
+
+# Returns (list(caller_id, birthday, comment, int(last_notified_year)), int(update_on_hour))
+def get_birthdays(caller_id: int):
+    global cur
+    res = cur.execute(f"SELECT * FROM {TABLE_BIRTHDAYS} WHERE caller_id=? ORDER BY birthday", (caller_id,)).fetchall()
+    res2 = cur.execute(f"SELECT update_on_hour FROM {TABLE_CALLERS} WHERE caller_id=?", (caller_id,)).fetchone()
     return res, res2
 
-def add_birthday(con: sql.Connection, cur: sql.Cursor, caller_id: str, date: str, comment: str):
+# Returns (list(caller_id, birthday, comment, int(last_notified_year)), list(caller_id, int(update_on_hour)))
+def get_all_birthdays():
+    global cur
+    res = cur.execute(f"SELECT * FROM {TABLE_BIRTHDAYS} ORDER BY birthday").fetchall()
+    res2 = cur.execute(f"SELECT * FROM {TABLE_CALLERS}").fetchall()
+    return res, res2
+
+def add_birthday(caller_id: str, date: str, comment: str) -> str:
+    global cur, con
     try:
         sql_date = datetime.datetime.strptime(date, DATE_FORMAT).strftime(SQL_DATE_FORMAT)
         caller_id = int(caller_id)
     except Exception as e:
-        print(f"strptime or caller_id thrown: {e}")
-        return
+        return f"Вводите дату в формате дд.мм.гг: {e}"
 
     if cur.execute(f"SELECT * FROM {TABLE_CALLERS} WHERE caller_id={caller_id}").fetchone() is None:
         DEFAULT_NOTIFICATION_TIME = 12
         cur.execute(f"INSERT INTO {TABLE_CALLERS} VALUES (?, ?)", (caller_id, DEFAULT_NOTIFICATION_TIME))
+    elif len(get_birthdays(caller_id)[0]) >= LIMIT_BIRTHDAYS_PER_CALLER:
+        return f"Слишком много дней рождения для одного человека/чата: {LIMIT_BIRTHDAYS_PER_CALLER}"
 
-    cur.execute(f"INSERT INTO {TABLE_BIRTHDAYS} VALUES (?, ?, ?)", (caller_id, sql_date, comment))
+    cur.execute(f"INSERT INTO {TABLE_BIRTHDAYS} VALUES (?, ?, ?, ?)", (caller_id, sql_date, comment, datetime.datetime.now().year - 1))
     con.commit()
-    print("inserted.")
+    return f"Добавлен день рождения для {comment} на дату {date}"
 
-def remove_birthday(con: sql.Connection, cur: sql.Cursor, caller_id: str, offset: str):
-    try:
-        offset = int(offset)
-        caller_id = int(caller_id)
-    except Exception as e:
-        print(f"offset or caller_id thrown: {e}")
-        return
-    
-    # what = f"DELETE FROM {TABLE_BIRTHDAYS} WHERE caller_id = {caller_id} LIMIT 1 OFFSET {offset}"
-    what = f"DELETE FROM birthdays WHERE rowid = (SELECT rowid FROM birthdays WHERE caller_id = {caller_id} ORDER BY birthday LIMIT 1 OFFSET {offset});"
+def actualize_birthday_last_notification(caller_id: str, offset: str):
+    global cur, con
+    what = f"UPDATE {TABLE_BIRTHDAYS} SET last_notified_year = {datetime.datetime.now().year} WHERE rowid = (SELECT rowid FROM {TABLE_BIRTHDAYS} WHERE caller_id = {caller_id} ORDER BY birthday LIMIT 1 OFFSET {offset});"
     cur.execute(what)
     con.commit()
-    print("removed if existed.")
 
-def init_db_if_needed(con: sql.Connection, cur: sql.Cursor):
+def remove_birthday(caller_id: str, offset: str, convert_from_user_friendly = False):
+    global cur, con
+    try:
+        offset = int(offset)
+        if convert_from_user_friendly:
+            offset -= 1
+        caller_id = int(caller_id)
+    except Exception as e:
+        return f"Неверные данные для удаления: {e}"
+
+    row = cur.execute(f"SELECT * FROM {TABLE_BIRTHDAYS} WHERE rowid = (SELECT rowid FROM {TABLE_BIRTHDAYS} WHERE caller_id = {caller_id} ORDER BY birthday LIMIT 1 OFFSET {offset});").fetchone()
+    if row is None:
+        return f"Неверный номер для удаления: {offset}"
+
+    len_before = len(get_birthdays(caller_id)[0])
+    what = f"DELETE FROM birthdays WHERE rowid = (SELECT rowid FROM birthdays WHERE caller_id = {caller_id} ORDER BY birthday LIMIT 1 OFFSET {offset});"
+    
+    cur.execute(what)
+    con.commit()
+    len_after = len(get_birthdays(caller_id)[0])
+    if len_before == len_after:
+        return "ERR: unexpected len_before != len_after"
+    return f"Удален день рождения {row[2]}"
+
+def init_db_if_needed():
+    global cur, con
+    con = sql.connect(BIRTHDAYS_DB_FILE_PATH)
+    cur = con.cursor()
     # https://sqlite.org/datatype3.html#date_and_time_datatype
-    cur.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_CALLERS}(caller_id INTEGER PRIMARY KEY, update_on INTEGER)")
-    cur.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_BIRTHDAYS}(caller_id INTEGER, birthday DATE, comment TINYTEXT, FOREIGN KEY (caller_id) REFERENCES caller(caller_id))")
+    cur.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_CALLERS}(caller_id INTEGER PRIMARY KEY, update_on_hour INTEGER)")
+    cur.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_BIRTHDAYS}(caller_id INTEGER, birthday DATE, comment TINYTEXT, last_notified_year INTEGER, FOREIGN KEY (caller_id) REFERENCES caller(caller_id))")
     con.commit()
 
 def main():
-    db_existed = os.path.exists(BIRTHDAYS_DB_FILE_PATH)
-    con = sql.connect(BIRTHDAYS_DB_FILE_PATH)
-    cur = con.cursor()
-    init_db_if_needed(con, cur)
+    DEBUG_CALLER = "123456789"
+
+    init_db_if_needed()
 
     user_input = ""
     while user_input != "exit":
@@ -68,11 +97,15 @@ def main():
             continue
         cmd = user_input_split[0]
         if cmd == "get":
-            print(get_birthdays(cur, DEBUG_CALLER))
+            print(get_birthdays(DEBUG_CALLER))
         if cmd == "add" and len(user_input_split) == 3:
-            add_birthday(con, cur, DEBUG_CALLER, user_input_split[1], user_input_split[2])
+            print(add_birthday(DEBUG_CALLER, user_input_split[1], user_input_split[2]))
         if cmd == "del" and len(user_input_split) == 2:
-            remove_birthday(con, cur, DEBUG_CALLER, user_input_split[1])
+            print(remove_birthday(DEBUG_CALLER, user_input_split[1]))
+        if cmd == "act":
+            count = len(get_birthdays(DEBUG_CALLER)[0])
+            for i in range(count):
+                actualize_birthday_last_notification(DEBUG_CALLER, i)
         if user_input == "exit":
             break
 
--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
-
 import logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
+logger = logging.getLogger(__name__)
 import sqlite3 as sql
 sql.threadsafety = 3 # https://docs.python.org/3/library/sqlite3.html#sqlite3.threadsafety
 import asyncio
-
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 from datetime import datetime
 
-logger = logging.getLogger(__name__)
+def birthday_logic(application: Application):
+    import birthday
+    bdays, callers = birthday.get_all_birthdays()
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    current_day = datetime.now().day
+    current_hour = datetime.now().hour
+    for index, bday in enumerate(bdays):
+        caller_id, bday_str, comment, last_notified_year = bday
+        if current_year >= last_notified_year:
+            continue
+        parsed_date = datetime.strptime(bday_str, birthday.SQL_DATE_FORMAT).date()
+        if parsed_date.month != current_month:
+            continue
+        if parsed_date.day != current_day:
+            continue
+        for caller in callers: # TODO: slow
+            if caller[0] == caller_id:
+                caller_hour = caller[1]
+                break
+        if caller_hour > current_hour:
+            continue
 
-last_returned_date = datetime.date()
+        # Finally we should send a message here:
+        Bot(application.bot).send_message(chat_id=caller_id, text=f"С днем рождения, {comment}!")
 
-async def birthdays():
-    WANT_MINUTE = 59
-    minute = datetime.now().minute
+        # Also increment year in db:
+        birthday.actualize_birthday_last_notification(caller_id, index)
+
+async def birthdays(application: Application):
+    import birthday
+    birthday.init_db_if_needed()
+    print("birthday_logic bootstrapped")
     while True:
-        print("Every 1 hour")
+        try:
+            birthday_logic(application)
+        except Exception as e:
+            print(e)
+            pass
         await asyncio.sleep(60 * 60)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -37,28 +65,71 @@ def is_caller_admin(update: Update) -> bool:
     # Source - https://stackoverflow.com/a/74736279
     # Posted by CallMeStag
     # Retrieved 2026-02-01, License - CC BY-SA 4.0
-
     chat_admins = update.effective_chat.get_administrators()
     return update.effective_user in (admin.user for admin in chat_admins)
 
 async def echo_ehh(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    print(update.message.chat_id, update.effective_user.id)
     text = update.message.text
     if text in ["эх", "эхх", "эххх", "эхххх", "эх)", "эх!", "эх."]:
         await update.message.reply_text("Эххх!")
+
+async def handle_bdays(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    import birthday
+    usage = """
+Команды:
+ /bday add дд.мм.гг Имечко Фамилия
+ /bday get
+ /bday delete номер
+ /bday help
+""".lstrip()
+    caller_id = update.effective_chat.id
+    text = update.message.text
+    split_text = text.split(maxsplit=3)[1:] # + remove /bday
+    if len(split_text) == 0 or split_text[0] == "help":
+        await update.message.reply_text(usage)
+        return
+    
+    cmd = split_text[0]
+    if cmd == "get":
+        bdays = birthday.get_birthdays(caller_id)
+        if bdays[1] is None:
+            await update.message.reply_text("В данном чате или лс нет дней рождения")
+        
+        resp = f"Уведомления приходят в ~{bdays[1][0]}:00 МСК:\n"
+        for index, bday in enumerate(bdays[0]):
+            caller_id, bday_str, comment, last_notified_year = bday
+            bday_str_converted = datetime.strptime(bday_str, birthday.SQL_DATE_FORMAT).strftime(birthday.DATE_FORMAT)
+            resp += f" {index + 1}: {bday_str_converted}, {comment}\n"
+        await update.message.reply_text(resp)
+        return
+    
+    if cmd == "add":
+        if len(split_text) != 3:
+            await update.message.reply_text("Использование: /bday add дд.мм.гг Имечко Фамилия")
+            return
+        await update.message.reply_text(birthday.add_birthday(caller_id, split_text[1], split_text[2].replace("@", "")))
+        return
+    if cmd == "delete" or cmd == "remove":
+        if len(split_text) != 2:
+            await update.message.reply_text("Использование: /bday delete номер")
+            return
+        await update.message.reply_text(birthday.remove_birthday(caller_id, split_text[1], True))
+        return
+
+    await update.message.reply_text("а?")
 
 def main() -> None:
     with open("SECRET.txt", "r") as secret_file:
         SECRET = secret_file.read().rstrip()
     application = Application.builder().token(SECRET).build()
     application.add_handler(CommandHandler("ai", echo))
+    application.add_handler(CommandHandler("bday", handle_bdays))
+    # COMMAND HANDLERS MUST BE BEFORE MESSAGE HANDLERS IN CODE.....
     application.add_handler(MessageHandler(filters.USER, echo_ehh))
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(birthdays())
+    loop.create_task(birthdays(application))
     application.run_polling(allowed_updates=Update.MESSAGE)
-    #application.run_polling(allowed_updates=Update.MESSAGE)
-    #loop.run_forever()
 
 if __name__ == "__main__":
     main()
